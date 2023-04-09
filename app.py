e3151242b9fbde9e85e5e383716199ae2b0cc0e6
Scott Waller <ssherwoodw@gmail.com>
2023-04-08 17:40:00 -0700

    added session cookies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -10,7 +10,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 
-responses = []
+RESPONSES_LIST = "responses"
 survey_length = len(survey.questions)
 
 @app.route("/")
@@ -21,17 +21,26 @@ def get_survey():
     
     return render_template("home.html", title=title, instructions=instructions)
     
+@app.route("/begin", methods=['POST'])
+def start_survey():
+    session[RESPONSES_LIST] = []
+    return redirect('/questions/0')
+
 
 
 @app.route("/questions/<int:qid>")
 def show_question(qid):
     """Show question/choices. Redirect if the URL is invalid."""
     question_num = qid
+    responses = session.get(RESPONSES_LIST)
+
+    if (responses is None):
+        return redirect("/")
 
     if question_num > len(responses):
         flash("That is an invalid request. Here is the next question:", "error")
-        return redirect(f"/questions/{len(responses)}")
-    
+        return redirect(f"/questions/{len('responses')}")
+
     question = survey.questions[qid].question
     choices = survey.questions[qid].choices
     
@@ -43,11 +52,15 @@ def show_question(qid):
 @app.route("/answer", methods=["POST"])
 def show_answers():
     answer = request.form['choice']
+    responses = session[RESPONSES_LIST]
     responses.append(answer)
+    session[RESPONSES_LIST] = responses
     if len(responses) == len(survey.questions):
         return redirect("/thanks")
     return redirect(f"/questions/{len(responses)}")
 
 @app.route("/thanks")
 def give_thanks():
-    return render_template("thanks.html")
+    
+    questions = survey.questions
+    return render_template("thanks.html", questions=questions)
